782e945b184d6dc2e35a78f59586ad2e0ee4db14
Caleb Wrigley <calebjosephwrigley@gmail.com>
2025-11-14 01:20:40 +0000

    Update articles.ts

--- client/src/lib/articles.ts
+++ client/src/lib/articles.ts
@@ -8,7 +8,7 @@
 import type { Article, ArticleMetadata } from "@/types/article";
 
 // Import example articles
-import ExampleArticle, { metadata as exampleMetadata } from "@/pages/articles/example-article";
+import iPhonePocket, { metadata as exampleMetadata } from "@/pages/articles/iphone-pocket";
 import GettingStarted, { metadata as gettingStartedMetadata } from "@/pages/articles/getting-started";
 
 /**
